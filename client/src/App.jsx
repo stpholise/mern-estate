1c94f1a8552c4bc63fd57ba746423ba03569fad9
stephen olise <stpholise@gmail.com>
2026-02-17 11:59:10 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>App</div>
-   )
- }
- 
- export default App
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn"; 
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
