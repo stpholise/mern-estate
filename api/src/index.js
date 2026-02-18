94e5ad9d5886825ceef8ebd9c732bf97c040b62f
stephen olise <stpholise@gmail.com>
2026-02-18 08:06:05 +0100

    creat user api route

--- api/src/index.js
+++ api/src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectDB } from "./lib/db.js"
+import userRouter  from './routes/user.route.js'
 
 dotenv.config()
 const PORT = process.env.PORT
 
 const app = express();
 
+app.use('/api/user', userRouter )
+
+ 
 
 app.listen(PORT, () => {
     console.log(`Server is connected to port    ${PORT}`)
