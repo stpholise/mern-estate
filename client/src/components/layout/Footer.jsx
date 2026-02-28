753109751b8d76b91446ec5299d03aaef975eda5
stephen olise <stpholise@gmail.com>
2026-02-28 22:41:18 +0100

    footer ui update

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,16 +1,111 @@
- 
+import { Link } from "react-router";
+import { Mail, User, Settings, Phone, MapPin } from "lucide-react";
+
+const iconMap = {
+  MapPin: MapPin,
+  user: User,
+  settings: Settings,
+  phone: Phone,
+  mail: Mail,
+};
 
 const Footer = () => {
   return (
-   <div className="h-100 bg-primary  w-full">
-        <div className="max-w-8xl mx-auto">
-          <div className="">
-            
+    <div className="min-h-100  px-2 sm:bg-primary py-2 w-full">
+      <div className="max-w-6xl  mx-auto rounded-2xl px-4 py-8 min-h-99 bg-primary h-full">
+        <div className=" ">
+          <div className="py-8 flex flex-col md:flex-row w-full justify-between gap-6">
+            <div className="flex-col flex gap-3 sm:gap-4 text-white">
+              <Link to="/">
+                <div className="w-18 h-12  rounded-md overflow-hidden object-center ">
+                  <img
+                    src="/abrss.png"
+                    alt="abrss max-h-12  w-20 object-cover "
+                  />
+                </div>
+              </Link>
+              <p className="text-sm text-white max-w-60">
+                Abrss is your trusted partner in real estate, dedicated to
+                helping you find the perfect property with ease and confidence.
+                Whether you're buying, selling, or renting, we provide premium
+                listings, expert guidance, and a seamless experience tailored to
+                your needs.
+              </p>
+            </div>
+            <div className="flex-col flex gap-3 sm:gap-4 text-white">
+              <h4 className="text-lg font-semibold mb-2">Sitemap</h4>
+              {navItems.map((item, index) => (
+                <Link key={index} to={item.url}>
+                  <button className="text-sm">{item.title} </button>
+                </Link>
+              ))}
+            </div>
+            <div className="flex-col flex gap-3 sm:gap-4 text-white">
+              <h4 className="text-lg font-semibold mb-2">Our Services</h4>
+              {services.map((item, index) => (
+                <p key={index} className="text-sm">
+                  {item}{" "}
+                </p>
+              ))}
+            </div>
+            <div className="flex-col flex gap-3 sm:gap-4 text-white">
+              <h4 className="font-semibold text-lg mb-2 ">Contact us</h4>
+              {contact.map((item, i) => {
+                const Icon = iconMap[item.icon];
+                return (
+                  <div key={i} className="flex items-center gap-2 text-sm">
+                    {Icon && <Icon size={18} />}
+                    <p>{item.text}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="border-t-gray-100 border-t py-8">
+            <p className="text-xs text-gray-200 text-center">© 2026 Abrss. All rights reserved.</p>
           </div>
         </div>
-
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+const navItems = [
+  {
+    url: "/",
+    title: "Home",
+  },
+  {
+    url: "/about",
+    title: "about",
+  },
+  {
+    url: "/Contact",
+    title: "contact",
+  },
+];
+
+const contact = [
+  {
+    icon: "MapPin",
+    text: "Abuja, Nigeria",
+  },
+  {
+    icon: "phone",
+    text: "+234 800 000 0000",
+  },
+  {
+    icon: "mail",
+    text: "info@abrss.com",
+  },
+];
+
+const services = [
+  "Property Buying",
+  "Property Selling",
+  "Property Rentals",
+  "Real Estate Consulting",
+  "Investment Advisory",
+];
+
+export default Footer;
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -4,42 +4,43 @@ import { useSelector } from "react-redux";
 
 const Header = () => { 
   const { currentUser } = useSelector((state) => state.user);
-
  
-
   return (
-     <div className=" fixed    top-4  left-0 right-0 flex z-100 bg-white/95 justify-between items-center max-w-6xl mx-auto p-3  rounded-4xl overflow-hidden px-8">
-      <Link to="/">
-        <div className="w-18 h-12  overflow-hidden object-center ">
+    <div className="fixed    top-4  left-0 right-0  z-50  bg-transparent px-2 ">
+      <div className=" flex z-50 bg-white/95 justify-between items-center max-w-6xl mx-auto p-3  rounded-4xl overflow-hidden px-8">
+        <Link to="/">
+          <div className="w-18 h-12  overflow-hidden object-center ">
           <img src="/abrss.png" alt="abrss max-h-12  w-20 object-cover " />
-        </div>
-      </Link> 
-      <nav className="flex items-center gap-4 lg:gap-6 font-medium  rounded-3xl py-1.5 px-8">
-        {navItems.map((item) => (
-          <Link
-            to={item.url}
-            key={item.url}
-            className="hidden sm:inline text-slate-700 "
-          >
-            {item.title}
-          </Link>
-        ))}
-        <Link to={currentUser ? "/profile" : "/signin"}>
-          {currentUser ? (
-            <img
-              className="rounded-full w-6 h-6 object-cover"
-              src={currentUser?.avatar || "/avatar.png"}
-              alt="profile"
-            />
-          ) : (
-            <button className="bg-slate-200 text-slate-800 ml-8 rounded-lg shadow-sm px-3 py-1">
-              Sign In
-            </button>
-          )}
+          </div>
         </Link>
-      </nav>
-    </div>
-   );
+        
+        <nav className="flex items-center gap-4 lg:gap-6 font-medium  rounded-3xl py-1.5 px-8">
+          {navItems.map((item) => (
+            <Link
+              to={item.url}
+              key={item.url}
+              className="hidden sm:inline text-slate-700 "
+            >
+              {item.title}
+            </Link>
+          ))}
+          <Link to={currentUser ? "/profile" : "/signin"}>
+            {currentUser ? (
+              <img
+                className="rounded-full w-6 h-6 object-cover"
+                src={currentUser?.avatar || "/avatar.png"}
+                alt="profile"
+              />
+            ) : (
+              <button className="bg-slate-200 text-slate-800 ml-8 rounded-lg shadow-sm px-3 py-1">
+                Sign In
+              </button>
+            )}
+          </Link>
+        </nav>
+      </div>
+  </div>
+  );
 };
 
 const navItems = [
